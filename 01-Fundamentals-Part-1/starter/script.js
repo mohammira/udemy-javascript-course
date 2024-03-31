45622ce7f8eb4ddcff3430def2067bef5e541273
Mira Mohammad <mohammira@gmail.com>
2024-03-31 16:48:22 +0800

    fundamentals part 1 #13

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+// let js = 'amazing';
+// console.log(1+2+3+4+5-6-7*8*9/10);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+// let x = 1;
+// const y = 2;
+// var z = 3;
+// x = 4
+// //y = 5;
+// z = 6;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// exponent operator
+console.log(2 ** 4);
+// dividing numbers equals a decimal
+console.log(10/3, 69/10);
+// string concat
+console.log('My' + ' ' + 'Name');
