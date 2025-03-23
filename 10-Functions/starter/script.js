09c3da413c5b6dbcd1d59e9e6f9e6b95d5ffe984
Mira Mohammad <mohammira@gmail.com>
2025-03-23 18:28:29 +0800

    section 10 challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// CHALLENGE #1
+const poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section 😃
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        let answer = prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`);
+        // if user clicks OK without any input, it will become '0' when casting answer to number
+        // below check added to handle this case
+        if (answer !== ''){
+            answer = Number(answer);
+        }
+        if (typeof answer === 'number' && answer > -1 && answer < this.options.length) {
+            this.answers[answer]++;
+        }
+
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array') {
+        if (type == 'array') {
+            console.log(this.answers);
+        }
+        else if (type == 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+
+    }
+}
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+// BONUS TEST DATA 1: [5, 2, 3]
+poll.displayResults.call({answers: [5, 2, 3]});
+poll.displayResults.call({answers: [5, 2, 3]}, 'string');
+
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]});
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string');
