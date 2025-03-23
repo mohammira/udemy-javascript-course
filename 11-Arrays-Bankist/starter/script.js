16ecc771e222050cc32acaf79a646b8fcd2e13ed
Mira Mohammad <mohammira@gmail.com>
2025-03-23 23:58:16 +0800

    section 11 challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,39 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// CHALLENGE #1
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each).
+For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's
+less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+*/
+
+function checkDocs(dogsJulia, dogsKate) {
+  // 1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs!
+  // So create a shallow copy of Julia's array, and remove the cat ages from that copied array
+  // (because it's a bad practice to mutate function parameters)
+  let dogsJuliaCopy = dogsJulia.slice();
+  dogsJuliaCopy.splice(0,1);
+  dogsJuliaCopy.splice(-2);
+  console.log(dogsJuliaCopy);
+
+  // 2. Create an array with both Julia's (corrected) and Kate's data
+  let allData = dogsJuliaCopy.concat(dogsKate)
+  console.log(allData);
+
+  // 3. For each remaining dog, log to the console whether it's an adult
+  // ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+  allData.forEach((dog, i) => {
+    if (dog < 3) {
+      console.log(`Dog number ${i+1} is still a puppy`);
+    }
+    else {
+      console.log(`Dog number ${i+1} is an adult and is ${dog} years old`);
+    }
+  })
+}
+
+checkDocs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
+checkDocs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
