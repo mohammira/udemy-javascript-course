f7780dd7e84332d9e68d01d86a33e9e91463f253
Mira Mohammad <mohammira@gmail.com>
2025-10-18 11:37:48 +0800

    change

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,8 +74,8 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-// CHALLENGE #1
-/*
+/* CHALLENGE #1
+
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each).
 For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's
 less than 3 years old.
@@ -110,3 +110,35 @@ function checkDocs(dogsJulia, dogsKate) {
 
 checkDocs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
 checkDocs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
+
+/////////////////////////////////////////////////
+/* CHALLENGE #2
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+function calcAverageHumanAge(ages) {
+  // 1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+  let humanAges = ages.map(dogAge => (dogAge <= 2 ? dogAge * 2 : 16 + dogAge * 4));
+  console.log("Dogs in human ages", humanAges);
+
+  // 2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+  let adultDogs = humanAges.filter(humanAge => humanAge >= 18);
+  console.log("All adult dogs", adultDogs);
+
+  // 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+  let averageAdultAge = adultDogs.reduce((acc, age) => acc + age, 0) / adultDogs.length;
+  console.log("Average adult age", averageAdultAge);
+}
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
